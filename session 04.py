44b4bec299d9f9c63dd08eec5525e08e6f7b72d3
Danial Hamidi . HD <148792344+Danielhamidi97@users.noreply.github.com>
2023-10-30 02:01:55 -0700

    Update session 04.py

--- session 04.py
+++ session 04.py
@@ -1,9 +1,9 @@
 persence = 30
 temp = 35
 
-print ( persence < temp)
-print (temp > persence)
-print (persence <= temp)
-print (persence >= temp)
-print (temp == persence)
-print (temp != persence)
+print ( persence < temp) #Ture
+print (temp > persence) #true
+print (persence <= temp) #true
+print (persence >= temp) #false
+print (temp == persence)# false
+print (temp != persence)# true
