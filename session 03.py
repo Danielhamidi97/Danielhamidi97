95ec572e9ff2524e544c3ad12a0e22d0e2992b1a
Danial Hamidi . HD <148792344+Danielhamidi97@users.noreply.github.com>
2023-10-30 01:43:10 -0700

    Add files via upload

---
+++ session 03.py
@@ -0,0 +1,14 @@
+Mid_score = input("Please enter your mid score: ")
+Mid_score = int(Mid_score)
+print("Your mid score is:", Mid_score)
+print(type(Mid_score))
+
+Final_score = int(input("Please enter your final score: "))
+print("Your final score is:", Final_score)
+print(type(Final_score))
+
+amir_score = Mid_score+Final_score
+print("Sum of your scores is:", amir_score)
+
+Average_score = amir_score/2
+print("Average of your scores is:", Average_score)
