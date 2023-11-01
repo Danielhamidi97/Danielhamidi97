093729df856fb1747f64088eb33e933b21535aeb
Danial Hamidi . HD <148792344+Danielhamidi97@users.noreply.github.com>
2023-11-01 00:49:41 -0700

    Add files via upload

---
+++ session 05.py.py
@@ -0,0 +1,39 @@
+team1 = int(input("dragon score: "))
+team2 = int(input("spirit score: "))
+team3 = int(input("scenario score: "))
+team4 = int(input("leopard score: "))
+team5 = int(input("buffalo score: "))
+
+if team1 >= 15:
+    print ("dragon`s with score of "+str(team1)+" has accepted the qualified!")
+else:
+    print ("dragon`s whit score of"+str(team1)+"has eliminated the qualified!")
+if team2 >= 15:
+    print ("spirit whit score of"+str(team2)+"has accepted the qualified!")
+else:
+    print("spirit whit score of "+str(team2)+"has eliminated the qualified!")
+if team3 >= 15:
+    print ("scenario with score of "+str(team3)+" has accepted the qualified!")
+else: 
+    print("scenario whit score of "+str(team3)+"has accepted the qualified!")
+if team4 >= 15:
+    print ("leopard whit score of "+str(team4)+"has accepted the qualified!")
+else:
+    print ("leopard whit score of"+str(team4)+"has eliminated the qualified!")
+if team5 >= 15:
+    print ("buffalo whit score of "+str(team5)+"has accepted the qualified!")
+else:
+    print ("buffalo whit score of "+str(team5)+"has eliminated the qualified!")
+    print ("End")
+
+
+    
+
+
+
+    
+
+
+    
+    
+           
