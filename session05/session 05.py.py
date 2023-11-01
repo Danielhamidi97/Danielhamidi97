755719d726f48f7f1acfedd75d000edfd84dff4f
Danial Hamidi . HD <148792344+Danielhamidi97@users.noreply.github.com>
2023-11-01 00:39:47 -0700

    Add files via upload

---
+++ session05/session 05.py.py
@@ -0,0 +1,39 @@
+team1 = int(input("team1 score: "))
+team2 = int(input("team2 score: "))
+team3 = int(input("team3 score: "))
+team4 = int(input("team4 score: "))
+team5 = int(input("team5 score: "))
+
+if team1 >= 15:
+    print ("team1 with score of "+str(team1)+" has accepted the qualified!")
+else:
+    print ("team 1 whit score of"+str(team1)+"has eliminated the qualified!")
+if team2 >= 15:
+    print ("team2 whit score of"+str(team2)+"has accepted the qualified!")
+else:
+    print("team2 whit score of "+str(team2)+"has eliminated the qualified!")
+if team3 >= 15:
+    print ("team1 with score of "+str(team3)+" has accepted the qualified!")
+else: 
+    print("team3 whit score of "+str(team3)+"has accepted the qualified!")
+if team4 >= 15:
+    print ("team4 whit score of "+str(team4)+"has accepted the qualified!")
+else:
+    print ("team4 whit score of"+str(team4)+"has eliminated the qualified!")
+if team5 >= 15:
+    print ("team5 whit score of "+str(team5)+"has accepted the qualified!")
+else:
+    print ("team5 whit score of "+str(team5)+"has eliminated the qualified!")
+    print ("End")
+
+
+    
+
+
+
+    
+
+
+    
+    
+           
