5415176cb381e4bd2c079b1e90182f6678a0cb81
Danial Hamidi . HD <148792344+Danielhamidi97@users.noreply.github.com>
2023-11-20 14:26:25 +0330

    Update Rock,paper,scissors.py

--- R,p,s/Rock,paper,scissors.py
+++ R,p,s/Rock,paper,scissors.py
@@ -2,21 +2,21 @@ import random
 
 user_win = 0
 computer_win = 0
-option =['r', 'p' , 's']
+option =['r ', 'p' , 's ']
 
 while True:
     user_input = input('type R / P / s or Q to quit:').lower()
 
-    if(user_input=='q') 
+    if(user_input=='q') break
     if(user_input not in option): continue
     random_no = random.randint(0 , 2)
     computer_pick = option[random_no]
-    if (user-input = 'r' and  computer_pick == 's') Or: (user-input = 'p' and  computer_pick == 'r') or (user-input = 's' and  computer_pick == 'p')
-        print ('you won!')
+    if (user-input = 'r' and  computer_pick == 's') Or: (user-input = 'p' and  computer_pick == 'r') or (user-input = 's' and  computer_pick == 'p'):
+        print ('you_won!')
         user_win += 1
     
     
-    elif (computer_pick = 'r' and  user_input == 's') Or: (computer_pick = 'p' and  user_input == 'r') or (computer_pick = 's' and  user_input == 'p')
+    elif (computer_pick = 'r' and  user_input == 's') Or: (computer_pick = 'p' and  user_input == 'r') or (computer_pick = 's' and  user_input == 'p'):
         print ('computer won!')
         computer_win  += 1
 
